fbad7edc20ae0e5e8d0730006b604dc7464f2630
Federico <federicoscutti@gmail.com>
2017-03-27 08:45:42 +0000

    choose njets in cuts

--- ssdilep/algs/algs.py
+++ ssdilep/algs/algs.py
@@ -152,7 +152,9 @@ class CutAlg(pyframe.core.Algorithm):
     
     #__________________________________________________________________________
     def cut_OneJet(self):
-        return self.chain.njets == 1
+      if hasattr(self.chain,"njets"): return self.chain.njets == 1
+      if hasattr(self.chain,"njet"):  return self.chain.njet == 1
+    
     #__________________________________________________________________________
     def cut_JetCleaning(self):
       for j in self.store['jets']:
@@ -2460,7 +2462,6 @@ class CutAlg(pyframe.core.Algorithm):
 
 
     def cut_PASS(self):
-      #print self.chain.njets
       return True
 
 
