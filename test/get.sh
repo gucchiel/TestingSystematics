a599c3684009aff81fff0d61a075894911e895b3
Federico <federicoscutti@gmail.com>
2017-04-13 03:17:03 +0000

    added test scripts

--- test/get.sh
+++ test/get.sh
@@ -1,40 +1,116 @@
 #!bin/bash
 
-python ../batch/SubmitGet.py --samp="PowhegPythia8EvtGen_AZNLOCTEQ6L1_DYmumu_1750M2000"
-python ../batch/SubmitGet.py --samp="PowhegPythia8EvtGen_AZNLOCTEQ6L1_DYmumu_3500M4000"
-python ../batch/SubmitGet.py --samp="PowhegPythia8EvtGen_AZNLOCTEQ6L1_DYmumu_4500M5000"
-python ../batch/SubmitGet.py --samp="Sherpa_NNPDF30NNLO_Zmumu_Pt1000_2000_CVetoBVeto"
-python ../batch/SubmitGet.py --samp="Sherpa_NNPDF30NNLO_Zee_Pt700_1000_CFilterBVeto"
-python ../batch/SubmitGet.py --samp="PowhegPythia8EvtGen_AZNLOCTEQ6L1_DYee_2000M2250"
-python ../batch/SubmitGet.py --samp="Sherpa_NNPDF30NNLO_Zmumu_Pt1000_2000_BFilter"
-python ../batch/SubmitGet.py --samp="Sherpa_221_NNPDF30NNLO_Zmumu_MAXHTPTV140_280_CFilterBVeto"
-python ../batch/SubmitGet.py --samp="Sherpa_NNPDF30NNLO_Zee_Pt700_1000_BFilter"
-python ../batch/SubmitGet.py --samp="Sherpa_NNPDF30NNLO_Wmunu_Pt700_1000_BFilter"
-python ../batch/SubmitGet.py --samp="Sherpa_NNPDF30NNLO_Zee_Pt2000_E_CMS_CVetoBVeto"
-python ../batch/SubmitGet.py --samp="Sherpa_221_NNPDF30NNLO_Zmumu_MAXHTPTV140_280_CVetoBVeto"
-python ../batch/SubmitGet.py --samp="Sherpa_221_NNPDF30NNLO_Zee_MAXHTPTV280_500_BFilter"
-python ../batch/SubmitGet.py --samp="Sherpa_NNPDF30NNLO_Zee_Pt1000_2000_CVetoBVeto"
-python ../batch/SubmitGet.py --samp="Sherpa_221_NNPDF30NNLO_Zee_MAXHTPTV70_140_BFilter"
-python ../batch/SubmitGet.py --samp="Sherpa_NNPDF30NNLO_Zee_Pt500_700_BFilter"
-python ../batch/SubmitGet.py --samp="Sherpa_NNPDF30NNLO_Wenu_Pt1000_2000_BFilter"
-python ../batch/SubmitGet.py --samp="PowhegPythiaEvtGen_P2012_SingleTopSchan_noAllHad_antitop"
-python ../batch/SubmitGet.py --samp="Sherpa_CT10_VV_qqmumu_500M1000"
-python ../batch/SubmitGet.py --samp="Sherpa_CT10_VV_llee_150M500"
-python ../batch/SubmitGet.py --samp="Sherpa_CT10_VV_qqmumu_150M500"
-python ../batch/SubmitGet.py --samp="Sherpa_CT10_VV_qqee_150M500"
-python ../batch/SubmitGet.py --samp="Sherpa_NNPDF30NNLO_Wenu_Pt140_280_BFilter"
-python ../batch/SubmitGet.py --samp="PowhegPythia8EvtGen_AZNLOCTEQ6L1_DYee_120M180"
-python ../batch/SubmitGet.py --samp="Sherpa_NNPDF30NNLO_Wenu_Pt140_280_CVetoBVeto"
-python ../batch/SubmitGet.py --samp="Sherpa_NNPDF30NNLO_Wenu_Pt500_700_BFilter"
-python ../batch/SubmitGet.py --samp="Sherpa_NNPDF30NNLO_Wmunu_Pt0_70_CFilterBVeto"
-python ../batch/SubmitGet.py --samp="PowhegPythiaEvtGen_P2012radLo_ttbar_hdamp172_up_nonallhad"
-python ../batch/SubmitGet.py --samp="PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad"
-python ../batch/SubmitGet.py --samp="Sherpa_NNPDF30NNLO_Wmunu_Pt500_700_BFilter"
-python ../batch/SubmitGet.py --samp="PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_dil"
-python ../batch/SubmitGet.py --samp="aMcAtNloHerwigppEvtGen_ttbar_nonallhad"
-python ../batch/SubmitGet.py --samp="Sherpa_CT10_VV_evmuv_150M500"
-python ../batch/SubmitGet.py --samp="Sherpa_CT10_lllljj_EW6"
-python ../batch/SubmitGet.py --samp="MadGraphPythia8EvtGen_A14NNPDF23LO_ttee_Np0"
+# from EXOT12data_v3nomi
+#python ../batch/SubmitGet.py --samp="00310370"
+
+# from EXOT12MC_v3n
+#python ../batch/SubmitGet.py --samp="PowhegPythiaEvtGen_P2012_Wt_dilepton_antitop"
+
+# from nHIGG3D3_v9
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH200"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH200"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH400"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH350"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH650"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH700"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH250"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH850"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH1000"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH800"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH1300"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH1050"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH1000"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH900"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH1250"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH1200"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH1250"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH750"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH300"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH250"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH450"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH500"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH650"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH350"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH550"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH600"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH950"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH700"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH800"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH200"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH600"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH900"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH1150"
+python ../batch/SubmitGet.py --samp="Pythia8EvtGen_A14NNPDF23LO_DCH950"
+
+
+
+  
+  
+  
+  
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
