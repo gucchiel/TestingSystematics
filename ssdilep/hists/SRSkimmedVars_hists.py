6ed8a9075ade3210bed9f9e17abbb882730619e1
Giulia Ucchielli <ucchielli@uibo-atlas-02.cr.cnaf.infn.it>
2017-05-30 15:34:34 +0200

    skimmed var set

---
+++ ssdilep/hists/SRSkimmedVars_hists.py
@@ -0,0 +1,24 @@
+from histconfig import *
+
+
+hist_list = []
+
+
+# -------
+# event
+# -------
+
+hist_list.append(h_PosCouple_mVis)
+hist_list.append(h_NegCouple_mVis)
+hist_list.append(h_Couples_mVis)
+hist_list.append(h_mOS1)
+hist_list.append(h_mOS2)
+hist_list.append(h_mOS3)
+hist_list.append(h_mOS4)
+# EOF
+
+
+
+
+
+
